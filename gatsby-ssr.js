df8037e25661841ae2d3d1f82fd4210755b0a85c
vrabe <git@vrabe.tw>
2021-06-21 00:57:23 +0800

    add the font modified Q

---
+++ gatsby-ssr.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { withPrefix } from "gatsby";
+
+// Load Modified Q font
+
+export const onRenderBody = ({ setHeadComponents }) => {
+
+    const regularPath = withPrefix("/fonts/ModifiedQ-Regular.woff2");
+    const boldPath = withPrefix("/fonts/ModifiedQ-Bold.woff2");
+    const css = `@font-face{font-family:"Modified Q";font-weight:400;src:url("${regularPath}")format("woff2");}@font-face{font-family:"Modified Q";font-weight:700;src:url("${boldPath}")format("woff2");}`;
+
+    setHeadComponents([
+        <link
+            key="font-modified-Q"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            href={regularPath}
+        />,
+        <link
+            key="font-modified-Q-bold"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            href={boldPath}
+        />,
+        <style dangerouslySetInnerHTML={{ __html: css }} />,
+    ]);
+
+};
